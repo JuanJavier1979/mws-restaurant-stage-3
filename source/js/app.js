81aae74de591e1a4ae9cb1a4011048928d6b20b5
JuanJavier1979 <jjmrestituto@gmail.com>
2018-06-23 16:44:21 +0200

    Add offline alert

--- source/js/app.js
+++ source/js/app.js
@@ -12,6 +12,33 @@ function registerSW() {
   }
 }
 
-window.addEventListener('load', function() {
+const connectionStatus = (connected) => {
+  if (connected) {
+    //console.log("we are ONLINE!");
+    let elAlert = document.getElementById('offlinealert');
+    if(elAlert) {
+      elAlert.parentElement.removeChild(elAlert);
+    }
+  } else {
+    //console.log("going offline!");
+    let offlineAlert = document.createElement('p');
+    offlineAlert.id = 'offlinealert';
+    offlineAlert.setAttribute('role', 'alert');
+    let offlineAlertText = document.createTextNode("Your connection is lost.");
+    offlineAlert.appendChild(offlineAlertText);
+    document.body.appendChild(offlineAlert);
+  }
+}
+
+window.addEventListener('load', () => {
   registerSW();
+  if(navigator.onLine) {
+    connectionStatus(true)
+  } else {
+    connectionStatus(false)
+  }
 });
+
+window.addEventListener('online', () => connectionStatus(true));
+
+window.addEventListener('offline', () => connectionStatus(false));
